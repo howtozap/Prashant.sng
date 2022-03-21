a06c8c9e0bf203b30da7146148a6cb9895b8b020
rocky <rockysinghsgrl@gmail.com>
2022-03-21 10:33:50 +0530

    sdfghj

---
+++ src/test/java/PractiseTest/Demo1Test.java
@@ -0,0 +1,13 @@
+package PractiseTest;
+
+import org.testng.annotations.Test;
+
+public class Demo1Test {
+
+	@Test
+	public void demo()
+	{
+		System.out.println("hi");
+		
+	}
+}
---
+++ src/test/java/PractiseTest/NokariTest.java
@@ -0,0 +1,28 @@
+package PractiseTest;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NokariTest {
+
+	@Test
+	public void test()
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.naukri.com/mnjuser/homepage");
+		List<WebElement> allLinks = driver.findElements(By.xpath("//a"));
+		allLinks.size();
+		for(WebElement link:allLinks)
+		{
+			String txt = link.getText();
+			System.out.println(txt);
+			
+		}
+		driver.close();
+	}
+}
